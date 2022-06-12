5436b588a52bbce549214921a20bf850c1c3f396
Aleksey Krichevskiy <boolivar@gmail.com>
2022-06-13 01:02:33 +0300

    DotRenderer throws

--- api/src/main/java/org/bool/integration/dot/api/DotRenderer.java
+++ api/src/main/java/org/bool/integration/dot/api/DotRenderer.java
@@ -2,8 +2,9 @@ package org.bool.integration.dot.api;
 
 import org.bool.integration.dot.api.model.IntegrationGraph;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 public interface DotRenderer {
-    void render(IntegrationGraph graph, OutputStream out);
+    void render(IntegrationGraph graph, OutputStream out) throws IOException;
 }
